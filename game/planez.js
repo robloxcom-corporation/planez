e17cecf9984884e3befe43fae678bb4b1e95a308
lolnuggets <46546150+lolnuggets@users.noreply.github.com>
2019-01-14 21:52:16 -0800

    Update planez.js

--- game/planez.js
+++ game/planez.js
@@ -3,7 +3,7 @@
 var canvas = document.getElementById("gamecanvas");
 var context = canvas.getContext("2d");
 
-context.fillStyle = "#000000"
+context.fillStyle = "#000000";
 context.fillRect(0,0,100,100);
 
 var button = document.createElement("button");
