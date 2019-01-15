cabc54f0e44e43a7b5054fd6b4fe96e7eb6d1afc
Blizard_Wizard <45040436+BlizardWizard@users.noreply.github.com>
2019-01-14 22:12:06 -0800

    Update planez.js

--- game/planez.js
+++ game/planez.js
@@ -1,8 +1,18 @@
+function draw() {
 
+  var canvas = document.getElementById("gamecanvas");
+  var context = canvas.getContext("2d");
 
-var canvas = document.getElementById("gamecanvas");
-var context = canvas.getContext("2d");
+  context.fillStyle = "#000000";
+  context.fillRect(0,0,100,100);
 
-context.fillStyle = "#000000";
-context.fillRect(0,0,100,100);
+  var image = new Image();
 
+  image.onload = function() {
+    context.drawImage(this,0,0);
+  };
+
+  image.src = "assets/sprites/lvl1 1.jpg"
+}
+
+draw();
