82e186e21102155a03759c12c7566b1b79bf239d
lolnuggets <46546150+lolnuggets@users.noreply.github.com>
2019-01-14 21:50:11 -0800

    Update planez.js

--- game/planez.js
+++ game/planez.js
@@ -9,7 +9,7 @@ context.fillRect(0,0,100,100);
 var button = document.createElement("button");
 button.innerHTML = "Do Something";
 
-var x = 0
+var x = 0;
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
