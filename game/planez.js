1be47f05f36ab87d9000b1b788d871920be65df1
lolnuggets <46546150+lolnuggets@users.noreply.github.com>
2019-01-14 21:55:52 -0800

    Update planez.js

--- game/planez.js
+++ game/planez.js
@@ -7,7 +7,7 @@ context.fillStyle = "#000000";
 context.fillRect(0,0,100,100);
 
 var button = document.createElement("button");
-button.innerhtml="nice";
+button.innerHTML = "Radical!";
 
 var x = 0;
 // 2. Append somewhere
