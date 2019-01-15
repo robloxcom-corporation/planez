4af2959f33f5f484fd0dbf07c7cde0a61398a68d
lolnuggets <46546150+lolnuggets@users.noreply.github.com>
2019-01-14 22:07:16 -0800

    Update planez.js

--- game/planez.js
+++ game/planez.js
@@ -7,15 +7,18 @@ context.fillStyle = "#000000";
 context.fillRect(0,0,100,100);
 
 var button = document.createElement("button");
-button.innerHTML = "Radical!";
+button.innerHTML = "Do Something";
 
-var x = 0;
+var x = 0
 // 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
 
 // 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("hello wrold");
+};
 
-button.addEventListener("click", function {
-  alert("hello world");                       
- });
+document.write(x);
 
 
