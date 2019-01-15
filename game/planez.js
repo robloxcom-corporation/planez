19a9321810f1956fb6305377dee0dfa6676c51dd
Blizard_Wizard <45040436+BlizardWizard@users.noreply.github.com>
2019-01-14 21:33:42 -0800

    Update planez.js

--- game/planez.js
+++ game/planez.js
@@ -3,3 +3,5 @@ document.write("Hello World. This is cool");
 var canvas = document.getElementById("gamecanvas");
 var context = canvas.getContext("2d");
 
+context.fillStyle = "#000000"
+context.fillRect(0,0,100,100)
